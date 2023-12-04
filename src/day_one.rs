98cddd3172d76e8c881fd7cc6cd8d84e7a5378b4
Stephen Sweeney <stephen.sweeney@jhuapl.edu>
2023-12-03 23:12:54 -0500

    Moved all string parsing to slicing, need to debug what's going on because I can't see what's wrong with this from the examples...

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,54 +1,114 @@
 //! Functions for day 1 of Advent of Code 2023
 
-use std::collections::linked_list;
 
 // constants for string based digits
 const NUM_STRS: [&'static str; 9] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
+const NUM_CHARS: [char; 9] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const MAX_DIGIT_LEN: usize = 5;
 
-pub fn get_str_config_value(line: &str) -> i32 {
-    // convert line into bytes
-    let line_bytes = line.as_bytes();
+pub fn get_part_2_config_value(line: &str) -> i32 {
+    // setup buffer vector
+    let mut buf: Vec<char> = Vec::new();
 
-    // setup first character and last character
-    let mut first_char = '\0';
-    let mut second_char = '\0';
+    // setup variables to hold the first and second digit
+    let mut first_digit = '\0';
+    let mut second_digit = '\0';
 
-    // loop over all the bytes
-    for idx in 0..line_bytes.len() {}
+    for (_idx, cur_char) in line.chars().enumerate() {
+        // print current setup
+        // println!("cur char = {}", cur_char);
+        // println!("cur buf = {:?}", buf);
+        // println!("cur buf len = {}", buf.len());
 
-    // FIXME: this is place holder so things will build
-    0
-}
+        // hold the digit here
+        let mut tmp_digit = '\0';
 
-/// Get the configuration values from a provided line, will find the first
-/// and last numeric character and convert those to an integer output
-pub fn get_config_value(line: &str) -> i32 {
-    let mut first_char = '\0';
-    let mut last_char = '\0';
-
-    for char in line.chars() {
-        if char.is_numeric() {
-            if first_char == '\0' {
-                first_char = char;
-            }
-            last_char = char;
+        // double check the size of the buffer
+        if buf.len() == MAX_DIGIT_LEN {
+            // println!("Buffer full, removing character");
+            buf.remove(0);
         }
+
+        // check to see if the current character is numeric
+        if cur_char.is_numeric() {
+            // println!("Found numeric character at {}, {}", idx, cur_char);
+            buf.clear();
+            // println!("cleared buf = {:?}", buf);
+            tmp_digit = cur_char;
+        } else {
+            // add value to the vector
+            buf.push(cur_char);
+            // println!("updated buf = {:?}", buf);
+
+            // test the buffer over each of the strings
+            for (num_idx, num_str) in NUM_STRS.iter().enumerate() {
+                // check to make sure that the number string is <= current buffer length
+                if num_str.len() <= buf.len() {
+                    // get the offset idx
+                    let start = buf.len() - num_str.len();
+
+                    // convert the buffer to a single string for comparison
+                    let buf_str = String::from_iter(&buf[start..buf.len()]);
+
+                    if buf_str == num_str.to_string() {
+                        // println!("Found valid digit str!!!!, {}", NUM_CHARS[num_idx]);
+                        buf.clear();
+                        tmp_digit = NUM_CHARS[num_idx];
+                        break;
+                    }
+                }
+            }
+        }
+
+        // update values if found a new digit
+        if tmp_digit != '\0' {
+            if first_digit == '\0' {
+                first_digit = tmp_digit;
+            }
+            second_digit = tmp_digit;
+        }
+
+        println!(
+            "first_digit = {}, second_digit = {}",
+            first_digit, second_digit
+        );
     }
 
-    if first_char == '\0' {
-        println!("How did you get here?");
-        0
-    } else if last_char == '\0' {
-        let char0 = first_char.to_string();
-        let char1 = first_char.to_string();
-        let num_str = char0 + &char1;
-        num_str.parse::<i32>().unwrap()
-    } else {
-        let char0 = first_char.to_string();
-        let char1 = last_char.to_string();
-        let num_str = char0 + &char1;
-        num_str.parse::<i32>().unwrap()
-    }
+    let char0 = first_digit.to_string();
+    let char1 = second_digit.to_string();
+    let final_num_str = char0 + &char1;
+    final_num_str.parse::<i32>().unwrap()
 }
+
+// Get the configuration values from a provided line, will find the first
+// and last numeric character and convert those to an integer output
+// pub fn get_config_value_part_1(line: &str) -> i32 {
+//     let mut first_char = '\0';
+//     let mut last_char = '\0';
+//
+//     for char in line.chars() {
+//         if char.is_numeric() {
+//             if first_char == '\0' {
+//                 first_char = char;
+//             }
+//             last_char = char;
+//         }
+//     }
+//
+//     if first_char == '\0' {
+//         println!("How did you get here?");
+//         0
+//     } else if last_char == '\0' {
+//         let char0 = first_char.to_string();
+//         let char1 = first_char.to_string();
+//         let num_str = char0 + &char1;
+//         num_str.parse::<i32>().unwrap()
+//     } else {
+//         let char0 = first_char.to_string();
+//         let char1 = last_char.to_string();
+//         let num_str = char0 + &char1;
+//         num_str.parse::<i32>().unwrap()
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 // use std::env;
 use std::fs;
-// mod day_one;
-
-const NUM_STRS: [&'static str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
-const NUM_CHARS: [char; 9] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// const MAX_DIGIT_LEN: i32 = 5;
+mod day_one;
 
 fn main() {
     // Read in file data
@@ -16,61 +10,22 @@ fn main() {
     // Start by splitting contexts by newlines
     let file_lines = contents.split("\n").collect::<Vec<&str>>();
 
-    // pull out a single line
-    let first_line = &file_lines[0];
+    let mut total = 0;
 
+    for (idx, cur_line) in file_lines.iter().enumerate() {
+        println!("-------------------------------------------------------------------------------");
+        println!("Current line = {}", cur_line);
 
-    // Need to reference string as bytes (making assumption that it's all ascii)
-    let line_bytes = first_line.as_bytes();
+        if "" != cur_line.to_string() {
+            let config_val = day_one::get_part_2_config_value(cur_line);
 
-    // Setup internal trackers for digits and indexing
-    let mut first_digit = '\0';
-    let mut second_digit;
-
-    let mut cur_char_hist = 0;
-
-    for idx in 0..line_bytes.len(){
-        println!("-------------------------------------------------------");
-        println!("cur line = {}", first_line);
-
-        // setup temp strings and characters based on current indexes
-        let tmp_char = line_bytes[idx] as char;
-        let tmp_str = String::from_utf8((line_bytes[idx - cur_char_hist..idx]).to_vec()).unwrap();
-
-        let mut tmp_digit = '\0';
-
-        println!("idx = {}", idx);
-        println!("cur_char_hist = {}", cur_char_hist);
-        println!("tmp_str = {}", tmp_str);
-        println!("tmp_char = {}", tmp_char);
-
-        // figure out if tmp_str is one of the numeric digits
-        let is_str = NUM_STRS.iter().position(|&r| r == tmp_str);
-
-        // check current character for numeric
-        if tmp_char.is_numeric() {
-            println!("Current char is numeric");
-            tmp_digit = tmp_char;
-            cur_char_hist = 1;
-        } else if is_str.is_some() {
-            println!("Current String is valid");
-            cur_char_hist = 0;
-            tmp_digit = NUM_CHARS[is_str.unwrap()];
-        } else {
-            cur_char_hist += 1;
-        }
-
-        if tmp_digit != '\0' {
-            println!("Found valid digit to use");
-            if first_digit == '\0' {
-                first_digit = tmp_digit;
-            }
-            second_digit = tmp_digit;
+            total += config_val;
 
             println!(
-                "first digit = {}, second digit = {}",
-                first_digit, second_digit
+                "line = {}, config_val = {}, total = {}",
+                idx, config_val, total
             );
         }
     }
+    println!("Final Total = {}", total);
 }
